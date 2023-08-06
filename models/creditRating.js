936d74486244a1ff13671ebd6ae4508725d28e2a
adrcotoe <adrcoto@yahoo.com>
2023-08-06 10:40:57 +0300

    added creditRating

---
+++ models/creditRating.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+const creditRatingSchema = mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    client_id: {
+        type: Schema.Types.ObjectId, ref: 'clients',
+    },
+    totalCreditScore: {
+        type: Number,
+    },
+    creditRating: {
+        type: String,
+        enum: ['Excellent Credit (Low Risk)', 'Good Credit (Moderate Risk)', 'Fair Credit (Moderate to High Risk)', 'Poor Credit (High Risk)', 'Possible creditworthy client (Upper Approximation)', 'Not creditworthy']
+    }
+});
+
+module.exports = mongoose.model('CreditRating', creditRatingSchema);
